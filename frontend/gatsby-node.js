2265c55b50b962c5c9a2b4221f53ba479ccc1cbc
shj <josh2kv@gmail.com>
2022-07-24 19:48:12 +0900

    add createPages

--- frontend/gatsby-node.js
+++ frontend/gatsby-node.js
@@ -1,9 +1,61 @@
-exports.createPages = async ({ actions }) => {
+exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
-  createPage({
-    path: "/using-dsg",
-    component: require.resolve("./src/templates/using-dsg.js"),
-    context: {},
-    defer: true,
+
+  const result = await graphql(`
+    {
+      categories: allStrapiCategory {
+        edges {
+          node {
+            id
+            name
+            description
+          }
+        }
+      }
+      products: allStrapiProduct {
+        edges {
+          node {
+            id
+            name
+            description
+            category {
+              name
+              description
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  if (result.errors) throw result.errors
+
+  const products = result.data.products.edges
+  const categories = result.data.categories.edges
+
+  products.forEach(product => {
+    createPage({
+      // 'i++'를 url에 제대로 적용하기 위해 encodeURIComponent 사용
+      path: `/${product.node.category.name.toLowerCase()}/${encodeURIComponent(
+        product.node.name.split(" ")[0]
+      )}`,
+      component: require.resolve("./src/templates/ProductDetail.js"),
+      context: {
+        id: product.node.id,
+        name: product.node.name,
+        category: product.node.category.name,
+      },
+    })
+  })
+  categories.forEach(category => {
+    createPage({
+      path: `/${category.node.name.toLowerCase()}`,
+      component: require.resolve("./src/templates/ProductList.js"),
+      context: {
+        id: category.node.id,
+        name: category.node.name,
+        description: category.node.description,
+      },
+    })
   })
 }
